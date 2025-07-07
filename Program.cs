d219d9bdfa288ccd99c53f2e5996f4ff0f5a2656
zinine73 <zinine@naver.com>
2025-07-07 14:01:18 +0900

    두수의 차

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@
     {
         // 이거...테스트
         //Solution0704(3, 2);
-        Console.WriteLine(Solution0707(3, 2));
+        Console.WriteLine(Solution0707(100, 2));
     }
 
     public static int Solution0707(int num1, int num2)
     {
-        return 0;
+        int answer = 0;
+        answer = num1 - num2;
+        return answer;
     }
 
     public static int Solution0704(int num1, int num2)
