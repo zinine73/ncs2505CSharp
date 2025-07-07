38ae78f29c2d2bb8064f51c48b07300335ba8c80
zinine73 <zinine@naver.com>
2025-07-07 13:16:15 +0900

    first

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // 이거...테스트
+        //Solution0704(3, 2);
+        Console.WriteLine(Solution0707(3, 2));
+    }
+
+    public static int Solution0707(int num1, int num2)
+    {
+        return 0;
+    }
+
+    public static int Solution0704(int num1, int num2)
+    {
+        int answer = 0;
+        answer = num1 % num2;
+        return answer;
+    }
+}
