e3d70374544fe9998fe7e0f48bed2bcfe6ff6423
zinine73 <zinine@naver.com>
2025-07-07 14:52:42 +0900

    comment

--- Program.cs
+++ Program.cs
@@ -1,18 +1,33 @@
 ﻿internal class Program
 {
+    /// <summary>
+    /// Main 함수
+    /// </summary>
+    /// <param name="args"></param>
     private static void Main(string[] args)
     {
-        // 이거...테스트
         //Solution0704(3, 2);
         Console.WriteLine(Solution07072(27, 19));
     }
 
+    /// <summary>
+    /// 두 수의 곱 구하기
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
     public static int Solution07072(int num1, int num2)
     {
         int answer = num1 * num2;
         return answer;
     }
 
+    /// <summary>
+    /// 빼기 연산
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
     public static int Solution0707(int num1, int num2)
     {
         int answer = 0;
@@ -20,6 +35,12 @@
         return answer;
     }
 
+    /// <summary>
+    /// 나머지 구하기
+    /// </summary>
+    /// <param name="num1"></param>
+    /// <param name="num2"></param>
+    /// <returns></returns>
     public static int Solution0704(int num1, int num2)
     {
         int answer = 0;
